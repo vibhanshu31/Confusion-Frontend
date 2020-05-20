81a27110a36ab1ceb8b50ca4af9094c52df32a7c
Vibhanshu Shandil <vibhanshushandil31@gmail.com>
2020-05-20 16:55:07 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './components/MainComponent';
+import './App.css';
 
-class App extends Component {
+class App extends React.Component {
 
   render() {
     return (
